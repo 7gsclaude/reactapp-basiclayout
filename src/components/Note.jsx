019cb73feda6820c8217c3f09017e29ab64d42e4
7gsclaude <96135428+7gsclaude@users.noreply.github.com>
2023-03-11 13:01:32 -0600

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1>title</h1>
+      <p>this is some content below the title in this div /note </p>
+    </div>
+  );
+}
+
+export default Note;
